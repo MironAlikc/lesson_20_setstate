78d480a78e52ce4ad0e9a673724f0c2a8d29e3b8
Aleksandr Myroniuk <miron.alikc@gmail.com>
2024-01-11 04:14:58 -0800

    set state

--- lib/presentation/widgets/custom_circle_avatar.dart
+++ lib/presentation/widgets/custom_circle_avatar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lesson_20_setstate/ui/theme/app_colors.dart';
+import 'package:lesson_20_setstate/presentation/theme/app_colors.dart';
 
 class CustomCircleAvatar extends StatelessWidget {
   const CustomCircleAvatar({
